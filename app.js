e70adb4c89afb56add09ad56cd281198eb0590c9
stu96534 <stu96534@gmail.com>
2022-09-11 14:16:50 +0800

    feat: add index detail main login register handlebars; add login register detail route

--- app.js
+++ app.js
@@ -2,16 +2,92 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+
 const app = express()
 const PORT = 3000
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialzed: true
+}))
+
+//首頁
 app.get('/', (req, res) => {
-  res.send('hallo world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
+
+//詳細
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+//登入
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+//登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+//註冊
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+//註冊檢查
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash =>
+        User.create({
+          name,
+          email,
+          password: hash
+        })
+      )
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+
+ 
+})
+
+app.post('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`APP is running on http://localhost:${PORT}`)
 })
---
+++ config/passport.js
@@ -0,0 +1,81 @@
+const passport = require('passport')
+const Localstrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+
+module.exports = app => {
+  //初始化 passport 模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+
+
+  //設定本地登入策略
+  passport.use(new Localstrategy({
+    usernameField: 'email',
+    passReqToCallback: true
+  }, (req, email, password, done) => {
+
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, req.flash('error', 'That email is not registered.'))
+        }
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('error', 'Email or Password incorrect.'))
+            }
+
+            return done(null, user)
+          })
+
+      })
+      .catch(err => done(err, false))
+  }))
+
+  //設定facebook登入策略
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)  //產生[鹽]，並設定複雜度係數為10
+            .then(salt => bcrypt.hash(randomPassword, salt)) // 為使用者密碼[加鹽]，產生雜湊值
+            .then(hash =>
+              User.create({
+                name,
+                email,
+                password: hash  //用雜湊值取代原本的使用者密碼
+              })
+            )
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }
+  ));
+
+  //設定序列化與反序列化
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+
+
+}
