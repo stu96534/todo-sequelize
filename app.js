de3479c4023850d4f6cde036a99619ac7ecd21fd
stu96534 <stu96534@gmail.com>
2022-09-11 19:03:07 +0800

    feat: setting env

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
 const usePassport = require('./config/passport')
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const routes = require('./routes')
 
 
@@ -15,7 +18,7 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialzed: true
 }))
